631cf05bad9ba96f07ad352f30b90bff521759ac
Rodrigo Costa <rodrigocostaful@gmail.com>
2021-08-02 18:19:34 -0300

    colectfast no apps

--- investimento/settings.py
+++ investimento/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-#AUTH_USER_MODEL = 'base.User'
+AUTH_USER_MODEL = 'core.User'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
