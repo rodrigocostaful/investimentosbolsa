42e16717173b2253d5d9dd48b0ce61d9c65802ba
Rodrigo Costa <rodrigocostaful@gmail.com>
2021-08-05 03:27:16 -0300

    listado fluxo cadastrados

--- investimento/fluxo/views.py
+++ investimento/fluxo/views.py
@@ -1,12 +1,45 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.urls import reverse_lazy
+from investimento.fluxo.models import Fluxo
 
 
 # Create your views here.
-def listar_fluxo(request):
 
-    data = {
-        'titulo' : "Lista de fluxo"
-    }
+class FluxoListViews(ListView):
+    model = Fluxo
+    template_name = 'fluxo/listar.html'
 
-    return render(request, 'fluxo/listar.html', data)
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            action = request.POST['action']
+            if action == 'searchdata':
+                data = []
+                position = 1
+                for i in Fluxo.objects.all():
+                    item = i.toJSON()
+                    item['position'] = position
+                    data.append(item)
+                    position += 1
+            else:
+                data['error'] = 'Ha ocurrido un error'
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data, safe=False)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Controle de fluxo'
+        context['title_conteudo'] = 'Fluxo de caixa'
+        context['list_url'] = reverse_lazy('website:fluxo-list')
+        context['breadcrumb'] = 'Fluxo'
+
+        return context
