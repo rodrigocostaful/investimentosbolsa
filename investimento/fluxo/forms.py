d37a521725189adba6ac5fcc8b4f362f39eb4515
Rodrigo Costa <rodrigocostaful@gmail.com>
2021-08-08 13:35:25 -0300

    fluxo com tabela via ajax

--- investimento/fluxo/forms.py
+++ investimento/fluxo/forms.py
@@ -43,14 +43,3 @@ class FluxoForm(ModelForm):
 
         }
 
-    def save(self, commit=True):
-        data = {}
-        form = super()
-        try:
-            if form.is_valid():
-                form.save()
-            else:
-                data['error'] = form.errors
-        except Exception as e:
-            data['error'] = str(e)
-        return data
